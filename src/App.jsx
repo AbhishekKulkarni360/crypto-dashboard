40bcdfbf63f9d938d34da9e66a27c949842d49ff
Abhishek Kulkarni <abhishekpramodkulkarni@gmail.com>
2025-07-15 17:51:56 +0530

    Connected CryptoChart to App and passed selectedCoin

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-import React, { useState } from 'react';
-import Navbar from './components/Navbar';
-import CryptoPrices from './components/CryptoPrices';
-import CryptoChart from './components/CryptoChart';
-import CryptoSearch from './components/CryptoSearch';
+import React, { useState } from "react";
+import CryptoSearch from "./components/CryptoSearch";
+import CryptoPrices from "./components/CryptoPrices";
+import CryptoChart from "./components/CryptoChart";
 
-function App() {
+const App = () => {
   const [selectedCoin, setSelectedCoin] = useState("bitcoin");
 
   return (
-    <>
-      <Navbar />
-      <main style={{ padding: '2rem', marginTop: '80px' }}>
-        <h2>Dashboard</h2>
-        <CryptoSearch onSearch={setSelectedCoin} />
-        <CryptoPrices coin={selectedCoin} />
-        <CryptoChart coin={selectedCoin} />
-      </main>
-    </>
+    <div style={{ padding: "2rem" }}>
+      <h1>Crypto Dashboard</h1>
+      <CryptoSearch onSearch={setSelectedCoin} />
+      <CryptoPrices coin={selectedCoin} />
+      <CryptoChart coin={selectedCoin} />
+    </div>
   );
-}
+};
 
 export default App;
